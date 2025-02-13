d42cf0c2a76e8b247df1f6d5d029296699c058b6
Juthamas66 <65010914608@msu.ac.th>
2025-02-13 17:40:07 +0700

    a

--- i/connectdb.php
+++ i/connectdb.php
@@ -1,10 +1,10 @@
 <?php 
 $host = "localhost";
 $user = "root";
-$pwd = "";
-$db = "shop";
+$pwd = "87654321";
+$db = "shop4608";
 
-$conn = mysqli_connect ($host, $user, $pwd) or die ("เชื่อมต่อฐานข้อมูลไม่ได้");
+$conn = mysqli_connect ($host, $user, $pwd, $db) or die ("เชื่อมต่อฐานข้อมูลไม่ได้");
 mysqli_select_db($conn, "shop")or die ("เลือกฐานข้อมูลไม่ได้");
 mysqli_query($conn, "set names utf8")
 ?>
