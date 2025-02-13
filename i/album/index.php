ac583bdac142e4fb1d9d25b394974a4f4f54dbe9
Juthamas66 <65010914608@msu.ac.th>
2025-02-13 19:14:07 +0700

    a

--- i/album/index.php
+++ i/album/index.php
@@ -174,7 +174,7 @@
     <div class="container">
       <a href="#" class="navbar-brand d-flex align-items-center">
         <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" aria-hidden="true" class="me-2" viewBox="0 0 24 24"><path d="M23 19a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h4l2-3h6l2 3h4a2 2 0 0 1 2 2z"/><circle cx="12" cy="13" r="4"/></svg>
-        <strong>ทิพย์นารี เพตาเสน (ตันหยง)</strong>
+        <strong>จุฑามาศ พรมวงศ์ (นุ่น)</strong>
       </a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
@@ -188,8 +188,8 @@
   <section class="py-5 text-center container">
     <div class="row py-lg-5">
       <div class="col-lg-6 col-md-8 mx-auto">
-        <h1 class="fw-light">อร่อยตั้งแต่รับประทานครั้งแรก</h1>
-        <p class="lead text-body-secondary">อร่อยตั้งแต่ครั้งแรกที่ลิ้มลอง รสชาติกลมกล่อมลงตัวในทุกคำที่รับประทาน.</p>
+        <h1 class="fw-light">เดลี่เบเกอรี้่</h1>
+        <p class="lead text-body-secondary">เบเกอรี่หอมกรุ่นจากเตา</p>
         <p>
           <a href="#" class="btn btn-primary my-2">Main call to action</a>
           <a href="#" class="btn btn-secondary my-2">Secondary action</a>
